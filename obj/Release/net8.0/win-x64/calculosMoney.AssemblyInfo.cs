ea2a3f01b9f703c716c35f1320a09a45dfd9bb28
batatinha465 <migueladri2006@gmail.com>
2024-11-10 02:02:34 -0300

    a

--- obj/Release/net8.0/win-x64/calculosMoney.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/calculosMoney.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculosMoney")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc852749b6239c23695dc4635343203a4fd327b")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculosMoney")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculosMoney")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
